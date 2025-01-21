fecbde267372f143e9ac49c257c9b24c6f15c1db
gbassaragh <30971014+gbassaragh@users.noreply.github.com>
2025-01-21 08:49:33 -0500

    Create MMM-SystemInfo.js

---
+++ MMM-SystemInfo.js
@@ -0,0 +1,42 @@
+Module.register("MMM-SystemInfo", {
+  defaults: {
+    updateInterval: 10000, // 10 seconds
+  },
+
+  start: function() {
+    this.systemInfo = {
+      cpuUsage: "--%",
+      cpuTemp: "--.-C",
+      ramUsage: "--%",
+      diskFree: "--.-GB",
+      internetStatus: "❌"
+    };
+    this.getData();
+    setInterval(() => {
+      this.getData();
+    }, this.config.updateInterval);
+  },
+
+  getData: function() {
+    this.sendSocketNotification("GET_SYSTEM_INFO");
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "SYSTEM_INFO") {
+      this.systemInfo = payload;
+      this.updateDom();
+    }
+  },
+
+  getDom: function() {
+    let wrapper = document.createElement("div");
+    wrapper.innerHTML = `
+      <div>CPU Usage: ${this.systemInfo.cpuUsage}</div>
+      <div>CPU Temp: ${this.systemInfo.cpuTemp}</div>
+      <div>RAM Usage: ${this.systemInfo.ramUsage}</div>
+      <div>Disk Free: ${this.systemInfo.diskFree}</div>
+      <div>Internet: ${this.systemInfo.internetStatus}</div>
+    `;
+    return wrapper;
+  }
+});
